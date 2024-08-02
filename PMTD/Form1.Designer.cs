4d7e03325c3bc7da58315178540a11b0d9ca5489
fatemevna <mohajerf43@gmail.com>
2024-08-03 02:54:58 +0330

    Project Created

---
+++ PMTD/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PMTD
+{
+    partial class tbOperation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelFirstNumber = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFirstNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBoxOperation = new System.Windows.Forms.TextBox();
+            this.tbSecondNumber = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // labelFirstNumber
+            // 
+            this.labelFirstNumber.AutoSize = true;
+            this.labelFirstNumber.Location = new System.Drawing.Point(83, 141);
+            this.labelFirstNumber.Name = "labelFirstNumber";
+            this.labelFirstNumber.Size = new System.Drawing.Size(86, 16);
+            this.labelFirstNumber.TabIndex = 0;
+            this.labelFirstNumber.Text = "First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(521, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Number:";
+            // 
+            // tbFirstNumber
+            // 
+            this.tbFirstNumber.Location = new System.Drawing.Point(86, 181);
+            this.tbFirstNumber.Name = "tbFirstNumber";
+            this.tbFirstNumber.Size = new System.Drawing.Size(225, 22);
+            this.tbFirstNumber.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(354, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Operation:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(354, 302);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(58, 16);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "                 ";
+            // 
+            // textBoxOperation
+            // 
+            this.textBoxOperation.Location = new System.Drawing.Point(357, 181);
+            this.textBoxOperation.Name = "textBoxOperation";
+            this.textBoxOperation.Size = new System.Drawing.Size(100, 22);
+            this.textBoxOperation.TabIndex = 5;
+            this.textBoxOperation.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // tbSecondNumber
+            // 
+            this.tbSecondNumber.Location = new System.Drawing.Point(524, 181);
+            this.tbSecondNumber.Name = "tbSecondNumber";
+            this.tbSecondNumber.Size = new System.Drawing.Size(225, 22);
+            this.tbSecondNumber.TabIndex = 6;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(348, 238);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(86, 30);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "=";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tbOperation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.tbSecondNumber);
+            this.Controls.Add(this.textBoxOperation);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbFirstNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelFirstNumber);
+            this.Name = "tbOperation";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFirstNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFirstNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBoxOperation;
+        private System.Windows.Forms.TextBox tbSecondNumber;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ PMTD/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PMTD
+{
+    public partial class tbOperation : Form
+    {
+        int result;
+        int firstnumber;
+        int secondnumber;
+
+        public tbOperation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            
+            bool isNumber = int.TryParse(tbFirstNumber.Text , out firstnumber);
+            if (isNumber == false)
+            {
+                tbFirstNumber.Text = "Enter a number moron!";
+            }
+            else
+            {
+                firstnumber = Int32.Parse(tbFirstNumber.Text);
+            }
+            bool isNumber2 = int.TryParse(tbSecondNumber.Text, out secondnumber);
+            if (isNumber2 == false)
+            {
+                tbSecondNumber.Text = "Enter a number moron!";
+            }
+            else
+            {
+                secondnumber = Int32.Parse(tbSecondNumber.Text);
+            }
+
+            
+            
+            
+           
+            if (textBoxOperation.Text == "+")
+            {
+                result = firstnumber + secondnumber;
+            }
+            else if (textBoxOperation.Text == "-")
+            {
+
+                result = firstnumber - secondnumber;
+            }
+            else if (textBoxOperation.Text == "/")
+            {
+                result = firstnumber / secondnumber;
+            }
+            else if ( textBoxOperation.Text == "*")
+            {
+                result = firstnumber * secondnumber;
+            }
+
+            if (result <= 50)
+            {
+                labelResult.ForeColor = Color.Red;
+            }
+            else if (result > 50)
+            {
+                labelResult.ForeColor = Color.Green;
+            }
+
+            labelResult.Text = result.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
